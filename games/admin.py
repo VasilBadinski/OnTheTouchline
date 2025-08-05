9286b5d3910dfd706d6cb4950f5e0fa9c72f87da
Vasil Badinski <162026826+VasilBadinski@users.noreply.github.com>
2025-08-05 15:06:09 +0300

    Hopefully a final push

--- games/admin.py
+++ games/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from games.models import Quiz, Question, QuizAttempt
 
-# Register your models here.
+
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(QuizAttempt)
+class QuizAttemptAdmin(admin.ModelAdmin):
+    pass
